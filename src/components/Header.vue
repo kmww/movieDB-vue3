03da52d7538ba791d715a693a473e3480792b39a
kmww <kmw9708@gmail.com>
2022-12-07 00:25:35 +0900

    Feat: "router 설정"

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <div class="nav">
+      <div v-for="nav in navigations" :key="nav.name" class="nav-item">
+        <RouterLink :to="nav.href" class="nav-link">
+          {{ nav.name }}
+        </RouterLink>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navigations: [
+        { name: "Home", href: "/" },
+        { name: "Movie", href: "/movie" },
+        { name: "About", href: "/about" },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "./Home";
+import About from "./About";
+import Movie from "./Movie";
+
+export default createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: "/",
+      component: Home,
+    },
+    {
+      path: "/about",
+      component: About,
+    },
+    {
+      path: "/movie",
+      component: Movie,
+    },
+  ],
+});
